e34de0b71a662e9fcf91ad9a19f3a78953ade6cb
Ec Maranan <marananemanuelle@gmail.com>
2023-08-19 02:50:48 +0800

    Initial Setup

---
+++ src/components/assets/Menu.js
@@ -0,0 +1,9 @@
+const Menu = () => {
+	return (
+		<div className="w-[50px] aspect-square">
+			<span>.</span>
+		</div>
+	);
+};
+
+export default Menu;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+	return (
+		<div className="grid place-items-center h-screen">
+			<div>
+				<p>hello~</p>
+				<h1>i am ec.</h1>
+				<p>front-end dev.</p>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
