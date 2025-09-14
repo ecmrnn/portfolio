6bb06c252d848aedff7fe16e106d048a576b4e74
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-09-14 17:38:55 +0800

    initial commit

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    
+    <div>
+        Hello, world!
+    </div>
+</template>
