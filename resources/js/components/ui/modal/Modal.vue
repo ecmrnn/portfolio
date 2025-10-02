af0e8095c886bb55f4fc207644c5f1a0afc48312
Emanuelle Maranan <marananemanuelle@gmail.com>
2025-10-03 00:32:00 +0800

    feat: added modal component and view certificate feature on certificate page

---
+++ resources/js/components/ui/modal/Modal.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// ...
+const props = defineProps({
+    show: Boolean
+});
+</script>
+
+<template>
+    <Transition name="fade">
+        <div :class="props.show ? 'opacity-100 grid' : 'hidden opacity-0 scale-0'"
+            class="h-screen overflow-hidden transition-all w-screen fixed inset-0 z-50 text-black bg-black/50 place-items-center">
+            <div :class="props.show ? 'opacity-100 scale-100' : 'opacity-0 scale-0'"
+                 class="m-5 md:max-w-1/3 p-5 bg-white md:min-w-2xl w-full transition-all">
+                <slot></slot>
+            </div>
+        </div>
+    </Transition>
+</template>
--- resources/js/pages/Certifications.vue
+++ resources/js/pages/Certifications.vue
@@ -1,30 +1,46 @@
 <script setup lang="ts">
 import LucideIcon from '@/components/LucideIcon.vue';
 import BaseLayout from '../layouts/BaseLayout.vue';
+import Button from '@/components/ui/button/Button.vue';
 import { Info, Database } from 'lucide-vue-next';
+import { Modal } from '@/components/ui/modal';
+import { ref } from 'vue';
+
 const certificates = [
     {
+        id: 1,
         title: 'Information Technology Specialists in JavaScript',
         description: 'This certificate helped me strengthen my understanding of JavaScript—especially things like functions, DOM manipulation, event handling, conditional logic, loops, and form validations.',
         achievedOn: '12/13/2023',
         examProvider: 'Certiport Pearson Vue',
-        icon: Database
+        icon: Database,
+        image: '/certificates/javascript.jpg'
     },
     {
+        id: 2,
         title: 'Information Technology Specialists in HTML and CSS',
         description: 'This certificate helped me strengthen my understanding of HTML and CSS—especially concepts like HTML5 semantic tags, forms, global attributes, font and layout styling, display types, mobile responsiveness, and other concepts used in modern web development.',
         achievedOn: '11/13/2023',
         examProvider: 'Certiport Pearson Vue',
-        icon: Database
+        icon: Database,
+        image: '/certificates/html-css.jpg'
     },
     {
+        id: 3,
         title: 'Information Technology Specialists in Databases',
         description: 'This certificate helped me strengthen my understanding of databases—especially things like SQL queries, table relationships, CRUD operations, and field data types.',
         achievedOn: '03/16/2022', 
         examProvider: 'Certiport Pearson Vue',
-        icon: Database
+        icon: Database,
+        image: '/certificates/databases.jpg'
     },
 ];
+
+const activeCertificate = ref<number|null>(null);
+
+const openCertificate = (id: number) => {
+    activeCertificate.value = id;
+} 
 </script>
 
 <template>
@@ -46,7 +62,7 @@ const certificates = [
             </div>
 
             <div class="overflow-auto">
-                <div class="p-5 lg:p-10 uppercase border-b-2 border-black dark:border-white last:border-b-0" v-for="certificate in certificates" :key="certificate.title">
+                <div class="p-5 lg:p-10 relative uppercase border-b-2 border-black dark:border-white last:border-b-0" v-for="certificate in certificates" :key="certificate.title">
                     <LucideIcon :icon="certificate.icon" class="mb-5" />
 
                     <h2 class="font-black text-2xl mb-5">{{ certificate.title }}</h2>
@@ -54,7 +70,21 @@ const certificates = [
                     <p class="text-sm mb-5">{{ certificate.description }}</p>
 
                     <p class="text-sm"><span class="font-black">Achieved on</span>: <time :datetime="certificate.achievedOn">{{ certificate.achievedOn }}</time></p>
-                    <p class="text-sm"><span class="font-black">Exam Provided by</span>: {{ certificate.examProvider }}</p>
+                    <p class="text-sm mb-5"><span class="font-black">Exam Provided by</span>: {{ certificate.examProvider }}</p>
+
+                    <Button class="uppercase" @click="openCertificate(certificate.id)">View Certificate</Button>
+
+                    <Modal :show="activeCertificate === certificate.id" @click.self="activeCertificate = null">
+                        <div>
+                            <div class="bg-[url('/public/svg/pattern.svg')] border border-black/10 mb-5 aspect-video w-full">
+                                <img :src="certificate.image" alt="certificate image" class="" />
+                            </div>
+                            <h3 class="font-black text-lg mb-5">{{ certificate.title }}</h3>
+                            
+                            <p class="text-sm">Achieved on: {{ certificate.achievedOn }}</p>
+                            <p class="text-sm">Exam provided by: {{ certificate.examProvider }}</p>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
